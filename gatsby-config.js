916e935392f6ce7f16d0ee621ef3294812ee6682
jcameronjeff <jcameronjeff@gmail.com>
2019-09-16 15:01:41 -0400

    first build

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Jeff Cameron`,
+    author: `Jeff Cameron`,
+    description: `Front End Development...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/devjefe`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/jcameronjeff`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,8 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+export default () => (
+  <Fragment>
+    Disclaimer: These are the rantings of a mid-level developer.
+  </Fragment>
+)
