420adeac2115eca25858a51d1b9c1b90a205f244
jcameronjeff <jcameronjeff@gmail.com>
2019-09-23 11:39:25 -0400

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     // with gatsby-plugin-theme-ui, the last theme in the config
     // will override the theme-ui context from other themes
     { resolve: `gatsby-theme-blog` },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-148454554-1",
+      },
+    },
   ],
   siteMetadata: {
     title: `Jeff Cameron`,
